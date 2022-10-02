17ff6c6dbd7c3818118f87f585bd0b23699634f8
AlexFoppa <alfoppa@hotmail.com>
2022-10-02 17:27:26 -0300

    adição de objeto e tabela audios

---
+++ audio.py
@@ -0,0 +1,7 @@
+class Audio:
+
+    def __init__(self, name, text) -> None:
+        self.id = 0
+        self.name = name
+        self.text = text
+        
--- database.py
+++ database.py
@@ -1,1 +1,33 @@
+
 import sqlite3
+
+conn = sqlite3.connect('audio.db')
+
+c = conn.cursor()
+
+def create_table():
+    with conn:
+        c.execute("""CREATE TABLE audios (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name text,
+            text text
+            )""")
+
+def insert_audio(audio):
+    with conn:
+        c.execute("INSERT INTO audios (name, text) VALUES (:name, :text)", {'name':audio.name, 'text':audio.text})
+        return c.lastrowid
+
+def get_all_audio():
+    c.execute("SELECT * FROM audios")
+    return c.fetchall()
+
+def get_audio_by_name(name):
+    c.execute("SELECT * FROM audios WHERE name=:name", {'name': name})
+    return c.fetchall()
+
+def get_audio_by_id(id):
+    c.execute("SELECT * FROM audios WHERE id=:id", {'id': id})
+    return c.fetchall()
+    
+conn.close()
